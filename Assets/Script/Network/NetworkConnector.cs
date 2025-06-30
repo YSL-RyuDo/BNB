3acf00ae2e06e3607cfce03e83118ffa5f18e321
YSL-RyuDo <84517476+YSL-RyuDo@users.noreply.github.com>
2025-06-30 13:28:09 +0900

    네트워크 스크립트

---
+++ Assets/Script/Network/NetworkConnector.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.IO;
+public class NetworkConnector : MonoBehaviour
+{
+    private TcpClient _client;
+    private NetworkStream _stream;
+
+    public TcpClient Client => _client;
+    public NetworkStream Stream => _stream;
+
+    public static NetworkConnector Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        TryConnectToServer();
+    }
+
+    private async void TryConnectToServer()
+    {
+        _client = new TcpClient();
+
+        try
+        {
+            await _client.ConnectAsync("127.0.0.1", 9000);
+            _stream = _client.GetStream();
+            StartListening();
+
+            Debug.Log("서버 연결 완료");
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("서버 연결 실패");
+        }
+    }
+
+    private async void StartListening()
+    {
+        byte[] buffer = new byte[1024];
+        StringBuilder completeMessage = new StringBuilder();
+
+        while (_client.Connected)
+        {
+            int byteCount = await _stream.ReadAsync(buffer, 0, buffer.Length);
+            if (byteCount == 0)
+                break;
+
+            string chunk = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            completeMessage.Append(chunk);
+
+            string messages = completeMessage.ToString();
+            string[] splitMessages = messages.Split('\n');
+
+            foreach (string msgRaw in splitMessages)
+            {
+                string msg = msgRaw.Trim();
+                if (!string.IsNullOrEmpty(msg))
+                {
+                    Debug.Log("서버 메시지 처리: " + msg);
+                    HandleServerMessage(msg);
+                }
+            }
+            completeMessage.Clear();
+            completeMessage.Append(splitMessages[splitMessages.Length - 1]);
+        }
+    }
+
+
+    private void HandleServerMessage(string message)
+    {
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        try
+        {
+            _stream?.Close();
+            _client?.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("소켓 닫기 실패: " + e.Message);
+        }
+    }
+}
